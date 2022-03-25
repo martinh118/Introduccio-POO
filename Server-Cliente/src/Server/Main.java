e35ca958320d680b399f2f1d2e36662aa7f9f68a
marcvr11 <94359544+marcvr11@users.noreply.github.com>
2022-03-25 20:07:07 +0100

    Add files via upload

---
+++ Server-Cliente/src/Server/Main.java
@@ -0,0 +1,254 @@
+package Server;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scan = new Scanner(System.in);
+    static char[][] tauler = new char[9][9];
+
+    public static void main(String[] args) {
+
+        mostrarTabla();
+
+        while(true){
+            actualizarTablaX();
+            buscarGanador();
+            actualizarTabla0();
+            buscarGanador();
+
+        }
+
+    }
+
+    public static void mostrarTabla(){
+
+        System.out.print("  ");
+        for(int n = 1;n  < tauler[0].length - 1; ++n) {
+            System.out.printf("%-3d", n );
+
+        }
+
+        System.out.println();
+
+        System.out.print("  ");
+
+
+        for(int i = 1; i < tauler.length - 1; ++i) {
+            for(int j = 1; j < tauler[0].length - 1; ++j) {
+                // Tabla con puntos
+                tauler[i][j] = '.';
+                System.out.print(tauler[i][j]  + "  ");
+            }
+            System.out.println();
+
+            if(i < tauler.length - 2) {
+                System.out.print("  ");
+            }
+        }
+
+    }
+
+    public static void actualizarTablaX(){
+
+        fichaX();
+
+        tablaActualizada();
+
+
+    }
+
+    public static void actualizarTabla0(){
+
+        ficha0();
+
+        tablaActualizada();
+
+
+    }
+
+    static void fichaX(){
+        int num = 0;
+
+        do{
+            System.out.println("Elige una fila: X");
+            num = scan.nextInt();
+        }while(num < 1 || num > 7);
+
+        int h = 0;
+        while(tauler[h+1][num] == '.'){
+            ++h;
+        }
+        if(h == 0){
+            System.out.println("Columna plena");
+        }
+        else{
+            tauler[h][num] = 'X';
+
+        }
+    }
+
+    static void ficha0(){
+        int num = 0;
+
+        do{
+            System.out.println("Elige una fila: 0");
+            num = scan.nextInt();
+        }while(num < 1 || num > 7);
+
+        int h = 0;
+        while(tauler[h+1][num] == '.'){
+            ++h;
+        }
+        if(h == 0){
+            System.out.println("Columna plena");
+        }
+        else{
+            tauler[h][num] = '0';
+
+        }
+    }
+
+    static void tablaActualizada(){
+        System.out.print("  ");
+        for(int n = 1;n  < tauler[0].length - 1; ++n) {
+            System.out.printf("%-3d", n );
+
+        }
+        System.out.println();
+
+        System.out.print("  ");
+
+        for(int i = 1; i < tauler.length - 1; ++i) {
+            for(int j = 1; j < tauler[0].length - 1; ++j) {
+                // Lletres
+                System.out.print(tauler[i][j]  + "  ");
+            }
+            System.out.println();
+
+            if(i < tauler.length - 2) {
+                System.out.print("  ");
+            }
+        }
+    }
+
+    public static void buscarGanador(){
+
+        comprobarVerticalX();
+        comprobarVertical0();
+
+        comprobarHorizontalX();
+
+        int linea = 0;
+/*
+
+    for (int i = 1; i < tauler.length -1; i++) {
+        for (int j = 1; j < tauler[0].length -1; j++) {
+
+
+            int hor = i;
+            int ver = j;
+            linea = 0;
+
+            //Comprobar linea horizontal
+            hor = i;
+            ver = j;
+            linea = 0;
+            while (tauler[hor+1][ver] == 'X') {
+                linea++;
+                hor++;
+                if (linea == 4) {
+                    System.out.println("4 en raya Vertical X");
+                }
+            }
+
+            hor = i;
+            ver = j;
+            linea = 0;
+            while (tauler[hor+1][ver] == '0') {
+                linea++;
+                hor++;
+                if (linea == 4) {
+                    System.out.println("4 en raya Vertical 0");
+                }
+            }
+
+
+        }
+    }
+    */
+
+    }
+
+
+    static void comprobarVerticalX(){
+        int linea = 0;
+
+        for (int i = 1; i < tauler.length -1; i++) {
+            for (int j = 1; j < tauler[0].length - 1; j++) {
+                //Comprobar linea vertical de X (FUNCIONA)
+                int hor = i;
+                int ver = j;
+                linea = 0;
+                while (tauler[hor+1][ver] == 'X') {
+                    linea++;
+                    hor++;
+                    if (linea == 4) {
+                        System.out.println("4 en raya Vertical X");
+                        System.exit(0);
+                    }
+                }
+            }
+        }
+
+    }
+
+    static void comprobarVertical0(){
+        int linea = 0;
+
+
+        for (int i = 1; i < tauler.length -1; i++) {
+            for (int j = 1; j < tauler[0].length - 1; j++) {
+                //Comprobar linea vertical de 0 (FUNCIONA)
+                int hor = i;
+                int ver = j;
+                linea = 0;
+                while (tauler[hor+1][ver] == '0') {
+                    linea++;
+                    hor++;
+                    if (linea == 4) {
+                        System.out.println("4 en raya Vertical 0");
+                        System.exit(0);
+                    }
+                }
+
+
+            }
+        }
+
+    }
+
+    static void comprobarHorizontalX(){
+        int linea = 0;
+
+
+        for (int i = 0; i < tauler.length -1; i++) {
+            for (int j = 0; j < tauler[0].length - 1; j++) {
+                //Comprobar linea horizontal de X
+                int hor = i;
+                int ver = j;
+                linea = 0;
+                while (tauler[hor][ver + 1] == 'X') {
+                    linea++;
+                    hor++;
+                    if (linea == 4) {
+                        System.out.println("4 en raya Vertical X");
+                        System.exit(0);
+                    }
+                }
+
+
+            }
+        }
+    }
+
+    }
+
---
+++ Server-Cliente/src/Server/Servidor/Server.java
@@ -0,0 +1,132 @@
+package Server.Servidor;
+
+import Server.model.Missatge;
+import Server.model.Numero;
+import Server.Main;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Server {
+
+    static ObjectInputStream in1;
+    static ObjectOutputStream out1;
+    static Socket s1;
+    static String j1 = "X";
+
+    static ObjectInputStream in2;
+    static ObjectOutputStream out2;
+    static Socket s2;
+    static String j2 = "0";
+
+    public static void main(String[] args) throws IOException {
+
+        conexion();
+        Main.mostrarTabla();
+        while(true){
+
+            peticionJugador1();
+            Main.actualizarTablaX();
+            Main.buscarGanador();
+            peticionJugador2();
+            Main.actualizarTabla0();
+            Main.buscarGanador();
+        }
+    }
+
+    static void conexion() throws IOException {
+
+        final int port = 5000;
+        ServerSocket Server;
+
+        //Conecta el servidor en otro servidor conocido.
+        s1 = new Socket("www.google.com", 80);
+        //Obtener direccion IP del servidor.
+        String ServerIP = s1.getLocalAddress().getHostAddress();
+
+
+        Server = new ServerSocket(port);
+        System.out.println("Server started at " + "\u001b[32m" + ServerIP + ":" + port);
+
+        try {
+            //Estara escuchando hasta que se conecte un segundo cliente
+
+                //Espero a que un cliente se conecte
+                s1 = Server.accept();
+                //Obtener datos enviados por el cliente
+                in1 = new ObjectInputStream(new BufferedInputStream(s1.getInputStream()));
+                out1 = new ObjectOutputStream(s1.getOutputStream());
+                System.out.println("Jugador 1 conectado");
+                Missatge jugador1 = new Missatge("Esperando a otro jugador...");
+                out1.writeObject(jugador1);
+
+
+                s2 = Server.accept();
+                //Obtener datos enviados por el cliente
+                in2 = new ObjectInputStream(new BufferedInputStream(s2.getInputStream()));
+                out2 = new ObjectOutputStream(s2.getOutputStream());
+                System.out.println("Jugador 2 conectado");
+                Missatge jugador2 = new Missatge("2 jugadores conectados.");
+                out2.writeObject(jugador2);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    static void peticionJugador1() throws IOException {
+
+
+        try {
+            //Envio mensaje cliente 2
+            String espera = "Es el turno del otro jugador.";
+            Missatge waiting = new Missatge(espera);
+            out2.writeObject(waiting);
+
+            //Envio mensaje a cliente 1
+            String mov = "¿En que columna quieres poner la ficha?";
+            Missatge movimiento = new Missatge(mov);
+            out1.writeObject(movimiento);
+
+            //Leo el mensaje que me envía
+            Numero mensCliente = (Numero) in1.readObject();
+            System.out.println(mensCliente.getNumero());
+
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    static void peticionJugador2() throws IOException {
+
+
+        try {
+            //Envio mensaje a cliente 1
+            String espera = "Es el turno del otro jugador.";
+            Missatge waiting = new Missatge(espera);
+            out1.writeObject(waiting);
+
+            //Envio mensaje cliente 2
+            String mov = "¿En que columna quieres poner la ficha?";
+            Missatge movimiento = new Missatge(mov);
+            out2.writeObject(movimiento);
+
+            //Leo el mensaje que me envía
+            Numero mensCliente = (Numero) in2.readObject();
+            System.out.println(mensCliente.getNumero());
+
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
